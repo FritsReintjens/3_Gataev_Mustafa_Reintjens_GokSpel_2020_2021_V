4b2e31c9e69149185f1949a5fbf4b841b4e989d2
Frits Reintjens <frits.reintjens@student.ucll.be>
2021-05-01 10:16:14 +0200

    spelerklasse en db

---
+++ src/model/database/SpelerDB.java
@@ -0,0 +1,56 @@
+package model.database;
+
+import model.domain.Speler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class SpelerDB {
+    Map<String, Speler> spelers;
+
+
+    public SpelerDB() throws FileNotFoundException {
+        this.spelers = new HashMap<>();
+        scanFile();
+    }
+
+    public void scanFile() {
+        try {
+            File spelersFile = new File("../../bestanden/speler.txt");
+            Scanner scanner = new Scanner(spelersFile);
+            while (scanner.hasNextLine()) {
+                String regel = scanner.nextLine();
+                List<String> lijst = Arrays.asList(regel.split(","));
+                Speler speler = new Speler(lijst.get(1), lijst.get(0), lijst.get(2), Double.parseDouble(lijst.get(3)));
+                spelers.put(speler.getSpelersnaam(),speler);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void saveFile(){
+        try{
+            FileWriter fileWriter = new FileWriter("../../bestanden/speler.txt");
+/*            Iterator<String> spelerIterator = spelers.keySet().iterator();
+            while(spelerIterator.hasNext()){
+                String key = spelerIterator.next();
+                Speler s = spelers.get(key);
+                fileriter.write(speler.toTextBestand());
+            }*/
+
+            for(Map.Entry mapElement : spelers.entrySet()){
+                Speler speler = (Speler)mapElement.getValue();
+                fileWriter.write(speler.toTextBestand());
+            }
+            fileWriter.close();
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
---
+++ src/model/domain/Speler.java
@@ -0,0 +1,86 @@
+package model.domain;
+
+import java.util.Objects;
+
+public class Speler {
+    private String voornaam, familienaam, spelersnaam;
+    private double goksaldo;
+
+    public Speler(){}
+
+    public Speler(String voornaam, String familienaam, String spelersnaam, double goksaldo){
+        setVoornaam(voornaam);
+        setFamilienaam(familienaam);
+        setSpelersnaam(spelersnaam);
+        setGoksaldo(goksaldo);
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        if (voornaam == null || voornaam.isEmpty())
+            throw new IllegalArgumentException("Voer een voornaam in.");
+        this.voornaam = voornaam;
+    }
+
+    public String getFamilienaam() {
+        return familienaam;
+    }
+
+    public void setFamilienaam(String familienaam) {
+        if (familienaam == null || familienaam.isEmpty()){
+            throw new IllegalArgumentException("Voer een familienaam in.");
+        }
+        this.familienaam = familienaam;
+    }
+
+    public String getSpelersnaam() {
+        return spelersnaam;
+    }
+
+    public void setSpelersnaam(String spelersnaam) {
+        if (spelersnaam == null || spelersnaam.isEmpty()){
+            throw new IllegalArgumentException("Voer een spelersnaam in.");
+        }
+        this.spelersnaam = spelersnaam;
+    }
+
+    public double getGoksaldo() {
+        return goksaldo;
+    }
+
+    public void setGoksaldo(double goksaldo) {
+        if (goksaldo < 0)
+            throw new IllegalArgumentException("Goksaldo moet positief zijn.");
+        this.goksaldo = goksaldo;
+    }
+
+    public String toTextBestand(){
+        return getFamilienaam() + "," + getVoornaam() + "," + getSpelersnaam() + "," + getGoksaldo();
+    }
+
+    @Override
+    public String toString() {
+        return "Speler{" +
+                "voornaam='" + voornaam + '\'' +
+                ", familienaam='" + familienaam + '\'' +
+                ", spelersnaam='" + spelersnaam + '\'' +
+                ", goksaldo=" + goksaldo +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Speler speler = (Speler) o;
+        return spelersnaam.equals(speler.spelersnaam);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(spelersnaam);
+    }
+}
