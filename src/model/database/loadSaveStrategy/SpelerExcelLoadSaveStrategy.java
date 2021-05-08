71de25ec9a8527f4433393d28b47c59ac0f3ae56
adamgataev <adam.gataev@student.ucll.be>
2021-05-08 03:00:39 +0200

    jars + start adapter

--- src/model/database/loadSaveStrategy/SpelerExcelLoadSaveStrategy.java
+++ src/model/database/loadSaveStrategy/SpelerExcelLoadSaveStrategy.java
@@ -1,17 +1,35 @@
 package model.database.loadSaveStrategy;
 
+import excel.ExcelPlugin;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Dit is een Adapter klasse
+ */
 public class SpelerExcelLoadSaveStrategy implements LoadSaveStrategy{
+    ExcelPlugin excelPlugin;
+
+    public SpelerExcelLoadSaveStrategy(){
+        excelPlugin = new ExcelPlugin();
+    }
 
     @Override
     public Map load(String filename) {
+        //read vraagt om een type File als parameter
+        //return excelPlugin.read(filename);
         return null;
     }
 
     @Override
     public void save(String filename, HashMap lijst) {
-
+        /*Collection<String> values = lijst.values();
+        ArrayList<String> listOfValues = new ArrayList<>(values);
+        File f = new File(filename+".xls");
+        excelPlugin.write(f,listOfValues);*/
     }
 }
