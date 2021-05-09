bd81461bf7e2b5ff8e22dff47045d9d5f52aca5e
Frits Reintjens <frits.reintjens@student.ucll.be>
2021-05-09 10:11:24 +0200

    story 3 setup

--- src/model/observer/OnderwerpInterface.java
+++
@@ -1,7 +0,0 @@
-package model.observer;
-
-public interface OnderwerpInterface {
-    public void registerObserver(Observer observer);
-    public void removeObserver(Observer observer);
-    public void notifyObserver(Observer observer);
-}
