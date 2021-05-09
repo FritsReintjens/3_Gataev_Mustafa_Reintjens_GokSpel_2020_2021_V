6244d8fee3cde608ad67e0f316ec2119c31d70e5
Frits Reintjens <frits.reintjens@student.ucll.be>
2021-05-09 11:06:12 +0200

    Story 3 WIP

--- src/controller/GamblerViewController.java
+++ src/controller/GamblerViewController.java
@@ -1,7 +1,24 @@
 package controller;
 
+import model.Spel;
+import model.observer.Observer;
 import view.GamblerView;
 
-public class GamblerViewController {
+public class GamblerViewController implements Observer {
     private GamblerView gamblerView;
+    private Spel spel;
+
+    public GamblerViewController (Spel spel){
+        this.spel = spel;
+        spel.registerObserver(this);
+    }
+
+    public void setGamblerView(GamblerView view){
+        this.gamblerView = view;
+    }
+
+    @Override
+    public void update(){
+
+    }
 }
